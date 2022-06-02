1930fae8c7f8a20632ddf163c489bcff9cf20ce5
alvo254 <alvinkhamron@gmail.con>
2022-06-02 09:59:39 +0300

    revamp

--- react-shopping/src/App.js
+++ react-shopping/src/App.js
@@ -2,7 +2,7 @@
 //import React from 'react'; // using class
 import logo from './logo.svg';
 import './App.css';
-import Header from './componets/Header';
+import Header from './components/Header';
 
 function App() {
   const name = "Alvo"
